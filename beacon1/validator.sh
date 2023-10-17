6ee7a38abd0fcd962045f0b34119706b3a860cce
Mart1i1n <mingfei.zh@outlook.com>
2023-10-17 06:38:59 +0000

    Update experiment

--- beacon1/validator.sh
+++ beacon1/validator.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-./validator --datadir=validatordata --accept-terms-of-use --interop-num-validators=666 --interop-start-index=0 --force-clear-db --chain-config-file=../config.yml --config-file=../config.yml
+./validator --datadir=validatordata --accept-terms-of-use --interop-num-validators=667 --interop-start-index=0 --force-clear-db --chain-config-file=../config.yml --config-file=../config.yml
