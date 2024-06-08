8b8e38df81c030f99d43aecbbcc4e471db41ce6a
xueqianLu <xueqian1991@gmail.com>
2024-06-08 22:41:28 +0800

    fix bug

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ export  DOCKER_BUILDKIT=1
 docker build -t geth:v1.13-base -f dockerfile/geth.Dockerfile .
 docker build -t attacker:latest -f dockerfile/attacker.Dockerfile .
 docker build -t beacon:base -f dockerfile/beacon.Dockerfile .
-docker build -t beacon:modified -f dockerfile/beacon.modified.Dockerfile .
+docker build -t beacon:modified -f dockerfile/beacon.modify.Dockerfile .
 docker build -t validator:base -f dockerfile/validator.Dockerfile .
-docker build -t validator:modified -f dockerfile/validator.modified.Dockerfile .
+docker build -t validator:modified -f dockerfile/validator.modify.Dockerfile .
