f5d913129d64c8f87729063464012f369d64097d
xueqianLu <xueqian1991@gmail.com>
2024-06-09 15:22:54 +0000

    update build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 export  DOCKER_BUILDKIT=1
-docker build -t geth:v1.13-base -f dockerfile/geth.Dockerfile .
-docker build -t attacker:latest -f dockerfile/attacker.Dockerfile .
-docker build -t beacon:base -f dockerfile/beacon.Dockerfile .
-docker build -t beacon:modified -f dockerfile/beacon.modify.Dockerfile .
-docker build -t validator:base -f dockerfile/validator.Dockerfile .
-docker build -t validator:modified -f dockerfile/validator.modify.Dockerfile .
+BUILDFLAG=""
+if [ "$REBUILD" = "1" ];then
+	BUILDFLAG="$BUILDFLAG --no-chace"
+fi
+docker build $BUILDFLAG -t geth:v1.13-base -f dockerfile/geth.Dockerfile .
+docker build $BUILDFLAG -t attacker:latest -f dockerfile/attacker.Dockerfile .
+docker build $BUILDFLAG -t beacon:base -f dockerfile/beacon.Dockerfile .
+docker build $BUILDFLAG -t beacon:modified -f dockerfile/beacon.modify.Dockerfile .
+docker build $BUILDFLAG -t validator:base -f dockerfile/validator.Dockerfile .
+docker build $BUILDFLAG -t validator:modified -f dockerfile/validator.modify.Dockerfile .
